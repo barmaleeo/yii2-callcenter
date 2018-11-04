d93bbb503fdcc62f70439ad1928b55b7ea5c3a86
pavel <s20305382@gmail.com>
2018-11-04 20:21:17 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	composer.json

---
+++ src/CallcenterModule.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavel
+ * Date: 11/4/18
+ * Time: 20:24
+ */
+
+namespace barmaleeo\callcenter;
+
+
+use yii\base\Module;
+
+class CallcenterModule extends Module
+{
+
+}
