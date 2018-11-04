05618e1eebe59ef22540d75d098e285ea83af6b9
pavel <s20305382@gmail.com>
2018-11-04 15:16:32 +0200

    initial commit

---
+++ bootstrap/CallcenterBootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pavel
+ * Date: 11/2/18
+ * Time: 21:23
+ */
+
+namespace barmaleeo\callcenter;
+
+use yii\base\BootstrapInterface;
+use yii\base\Application;
+
+class MyBootstrapClass implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        $app->on(Application::EVENT_BEFORE_REQUEST, function () {
+            // do something here
+        });
+    }
+}
