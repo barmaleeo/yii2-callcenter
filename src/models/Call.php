91c8d21280ec27b1ef6e9ce61da00beebf5d5800
pavel <s20305382@gmail.com>
2018-12-17 01:37:55 +0200

    Логгирование

--- src/models/Call.php
+++ src/models/Call.php
@@ -8,6 +8,8 @@ use Yii;
  * This is the model class for table "call".
  *
  * @property int $id
+ * @property string $uuid
+ * @property int $direction
  * @property int $type_id
  * @property int $phone_id
  * @property int $status_id
@@ -21,11 +23,27 @@ use Yii;
  */
 class Call extends \yii\db\ActiveRecord
 {
-    const STATUS_READY  = 0;
 
+    const STATUS_READY              = 0;   //  status = 0 - введен, активный статус
+    const STATUS_RINGING            = 64;  //  status = 64 - входящий звонок
+    const STATUS_CALLING            = 72;  //  status = 72 - старт вызова
+    const STATUS_RECORD_MSG         = 73;  //  status = 73 - запись на автоответчик
+    const STATUS_TALKING            = 96;  //  status = 96 - взята оператором
+    const STATUS_CONN_ERROR         = 97;  //  status = 97  - ошибка соединения
+    const STATUS_CLOSED_SYS         = 98;  //  status = 98  - снят системой
+    const STATUS_NO_MEDIA           = 121; //  status = 121 - ошибка соединения - нажата кнопка "Не слышу абонента"
+    const STATUS_MSG_BLOCKED        = 122; //  status = 122 - "абонент заблокирован"
+    const STATUS_MSG_NO_ANSWER      = 123; //  status = 123 - "абонент не отвечает"
+    const STATUS_NO_ANSWER          = 124; //  status = 124 - не отвечает
+    const STATUS_BUSY               = 125; //  status = 125 - линия заняиа
+    const STATUS_COMPLETED_CLIENT   = 126; //  status = 126 - завершен клиентом
+    const STATUS_COMPLETED          = 127; //  status = 127 - завершен оператором
+    const STATUS_CLOSED             = 128; //  status = 128 - закрыт супервайзером
+    const STATUS_UNANSWERED         = 129; //  status = 129 - неотвечен
+    const STATUS_AUTOANSWER_MSG     = 130; //  status = 130 - есть сообщение автоответчика
 
-    const DIRECTION_UNCALL  = 0;
-    const DIRECTION_OUTCALL = 1;
+    const DIRECTION_OUTCALL = 0;
+    const DIRECTION_INCALL  = 1;
     const DIRECTION_LOCAL   = 2;
 
     public $name;
@@ -46,8 +64,8 @@ class Call extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['type_id', 'phone_id', 'status_id', 'op_id', 'goal'], 'integer'],
-            [['enable_time', 'start_time', 'end_time', 'created', 'updated'], 'safe'],
+            [['type_id', 'direction', 'phone_id', 'status_id', 'op_id', 'goal'], 'integer'],
+            [['enable_time', 'uuid'], 'safe'],
         ];
     }
 
@@ -89,13 +107,54 @@ class Call extends \yii\db\ActiveRecord
             ->leftJoin('user','user.id=user_phone.user_id')
             ->groupBy('call.id');
     }
+    public static function startIncall($params){
+        
+        if(isset($params['phone']) && strlen($params['phone']) == 10 ) {
+
+            $clientClass = Yii::$app->params['callcenter']['client']['class'];
+            
+            $clients = $clientClass::findByPhoneNumber($params['phone']);
+
+            if(count($clients) == 0){
+                // создаем нового клиента по входящему звонку
+                $client = $clientClass::createByPhoneCall($params['phone']);
+                $callType = 1000;
+                
+            } else if(count($clients) == 1){
+                // Пользователь определился однозначно
+                $client = $clients[0];
+                $callType = 1005;
+            }else{
+                // Определилось  несколько пользователей
+                $client = $clients[0];
+                $callType = 1005;
+            }
+                $call = new static();
+                $call->status_id    = static::STATUS_RINGING;
+                $call->phone_id     = $client->phone_id;
+                $call->direction    = static::DIRECTION_INCALL;
+                $call->type_id      = $callType;
+                $call->uuid         = $params['uuid'];
+                $call->save();
+
+                $stats = [
+                    'id'        => $client->id,
+                    'call_id'   => $call->id,
+                    'name'      => $client->name,
+                ];
+
+            return $stats;
+        }
+
+    }
     
     public static function finishCall($callUuid){
         if($call = parent::findOne(['call_id' => $callUuid])){
 
             $log = new CallLog();
             $log->call_id = $call->id;
-            
+            $log->event_id = CallLog::CALL_EVENT_FINISH;
+            $log->save(false);
         }
 
     }
---
+++ src/models/CallLog.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace barmaleeo\callcenter\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "call_log".
+ *
+ * @property int $id
+ * @property int $call_id
+ * @property int $event_id
+ * @property string $comment
+ * @property string $created
+ */
+class CallLog extends \yii\db\ActiveRecord
+{
+    const CALL_EVENT_ACTIVE                 = 0;
+    const CALL_EVENT_ERROR                  = 1;
+    const CALL_EVENT_CALLING                = 2; // progress
+    const CALL_EVENT_BUSY                   = 3; // busy
+    const CALL_EVENT_NO_ANSWER              = 4; // no_answer
+    const CALL_EVENT_ACCEPTED               = 10;
+    const CALL_EVENT_CLOSE_CALL             = 15;
+    const CALL_EVENT_BYE                    = 13;
+    const CALL_EVENT_MUTED                  = 17;
+    const CALL_EVENT_UNMUTED                = 18;
+    const CALL_EVENT_START_VOICEMAIL        = 19;
+    //const CALL_EVENT_RINGING                = 0;
+    const CALL_EVENT_OUTGOING_TERMINATED    = 301;
+    const CALL_EVENT_INCALL_REJECTED        = 302;
+    const CALL_EVENT_FINISH                 = 307;
+    const CALL_EVENT_END_VOICEMAIL          = 308;
+
+
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'call_log';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['call_id', 'event_id'], 'integer'],
+            [['comment'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('common', 'ID'),
+            'call_id' => Yii::t('common', 'Call ID'),
+            'event_id' => Yii::t('common', 'Event ID'),
+            'comment' => Yii::t('common', 'Comment'),
+            'created' => Yii::t('common', 'Created'),
+        ];
+    }
+}
